9bea56dd0f50c105d795e7d0962c7d89dbeb6091
di3g0xh <diego.adaos01@alumnos.ucn.cl>
2025-09-28 17:25:19 -0300

    cambios de diseño (bolaMagica)

--- bolaMagicaApi/scriptBolaMagica.js
+++ bolaMagicaApi/scriptBolaMagica.js
@@ -1,10 +1,17 @@
 const preguntaInput = document.getElementById('pregunta-magica')
 const botonPregunta = document.getElementById('ask-button')
-const cartelNegativo = document.getElementById('cartel-negativo').querySelector('p')
-const cartelPositivo = document.getElementById('cartel-positivo').querySelector('p')
-const cartelNeutral = document.getElementById('cartel-neutral').querySelector('p')
 
-let respuestasPositivas=[]
+// divs
+const cartelNegativo = document.getElementById('cartel-negativo')
+const cartelPositivo = document.getElementById('cartel-positivo')
+const cartelNeutral = document.getElementById('cartel-neutral')
+
+// p
+const cartelNegativoP = document.getElementById('cartel-negativo').querySelector('p')
+const cartelPositivoP = document.getElementById('cartel-positivo').querySelector('p')
+const cartelNeutralP = document.getElementById('cartel-neutral').querySelector('p')
+
+let respuestasPositivas = []
 let respuestasNegativas = []
 let respuestasNeutras = []
 
@@ -20,15 +27,19 @@ async function fetchRespuestasPorCategoria(){
 
     }catch(error){
         console.error("No se pudieron cargar las categorías de respuestas.", error)
-        cartelNeutral.textContent = "No se pudo conectar con la Bola 8."
+        cartelNeutralP.textContent = "No se pudo conectar con la Bola 8."
+        cartelNeutral.classList.remove('hidden')
     }
-
 }
 
-function resetearContenidoCarteles() {
-    cartelNegativo.textContent = ''
-    cartelPositivo.textContent = ''
-    cartelNeutral.textContent = ''
+function resetearContenidoCarteles(){
+    cartelNegativo.classList.add('hidden')
+    cartelPositivo.classList.add('hidden')
+    cartelNeutral.classList.add('hidden')
+
+    cartelNegativoP.textContent = ''
+    cartelPositivoP.textContent = ''
+    cartelNeutralP.textContent = ''
 }
 
 async function obtenerRespuestaDeLaBola() {
@@ -38,10 +49,12 @@ async function obtenerRespuestaDeLaBola() {
         return;
     }
     resetearContenidoCarteles();
-    cartelNeutral.textContent = "Pensando..."
-            
-    try {
-        const url = 'https://corsproxy.io/?' + encodeURIComponent('https://www.eightballapi.com/api?locale=es')
+    
+    cartelNeutralP.textContent = "Pensando..."
+    cartelNeutral.classList.remove('hidden')
+
+    try{
+        const url = 'https://corsproxy.io/?' + encodeURIComponent('https://www.eightballapi.com/api?locale=es');
         const response = await fetch(url);
         const data = await response.json();
         const respuestaDeLaBola = data.reading;
@@ -49,29 +62,24 @@ async function obtenerRespuestaDeLaBola() {
         resetearContenidoCarteles();
 
         if (respuestasPositivas.includes(respuestaDeLaBola)) {
-            cartelPositivo.textContent = respuestaDeLaBola
+            cartelPositivoP.textContent = respuestaDeLaBola
+            cartelPositivo.classList.remove('hidden')
 
-        }else if(respuestasNegativas.includes(respuestaDeLaBola)) {
-            cartelNegativo.textContent = respuestaDeLaBola
-        }else{
-            cartelNeutral.textContent = respuestaDeLaBola
+        } else if (respuestasNegativas.includes(respuestaDeLaBola)) {
+            cartelNegativoP.textContent = respuestaDeLaBola
+            cartelNegativo.classList.remove('hidden')
+
+        } else {
+            cartelNeutralP.textContent = respuestaDeLaBola
+            cartelNeutral.classList.remove('hidden')
         }
 
-        } catch (error) {
-            resetearContenidoCarteles();
-            cartelNeutral.textContent = `Error: ${error.message}`
+    }catch(error){
+        resetearContenidoCarteles()
+        cartelNeutralP.textContent = `Error: ${error.message}`
+        cartelNeutral.classList.remove('hidden')
     }
-}
-
-const navToggle = document.getElementById("nav-toggle");
-const navMenu = document.getElementById("nav-menu");
-
-if (navToggle && navMenu) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("hidden");
-  });
 }
 
 document.addEventListener("DOMContentLoaded", fetchRespuestasPorCategoria)
 botonPregunta.addEventListener("click", obtenerRespuestaDeLaBola)
-
