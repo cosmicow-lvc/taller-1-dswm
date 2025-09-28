9139196eb6311fd059602ec3a785b32f9f785406
NoIdeaA <182179166+NoIdeaA89@users.noreply.github.com>
2025-09-28 18:53:33 -0300

    a

--- bolaMagicaApi/scriptBolaMagica.js
+++ bolaMagicaApi/scriptBolaMagica.js
@@ -81,5 +81,14 @@ async function obtenerRespuestaDeLaBola() {
     }
 }
 
+const navToggle = document.getElementById("nav-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+if (navToggle && navMenu) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("hidden");
+  });
+}
+
 document.addEventListener("DOMContentLoaded", fetchRespuestasPorCategoria)
 botonPregunta.addEventListener("click", obtenerRespuestaDeLaBola)
